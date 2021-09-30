ae1755073671971441bcd1bf9d7db2a3def07682
AsifAl23 <capinnemo@hotmail.com>
2021-09-30 15:55:18 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5424a6e9-0115-45d8-9938-fad20aa4dbc4';
+export const MODAL_CLOSE_SEC = 2.5;
